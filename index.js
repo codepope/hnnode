bf07e535adbe01a668fcecbc0fd71634b4ca97ac
Dj Walker-Morgan <djwm@mongodb.com>
2019-01-21 17:15:31 +0000

    Stop backpressuring the stream to death

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 #!/usr/bin/env node
 
-const argv = require('yargs').default("word","mongodb").argv
+const argv = require("yargs").default("word", "mongodb").argv;
 
 const { IncomingWebhook } = require("@slack/client");
 
-var hyperquest = require("hyperquest");
+const stream = require("stream");
+
+var request = require("request");
 var ndjson = require("ndjson");
 var through2 = require("through2");
 var filter = require("through2-filter");
 var spy = require("through2-spy");
-var strm = require("stream");
 var entities = require("entities");
-var slackify= require("slackify-html");
-
+var slackify = require("slackify-html");
 
 var webhookurl = process.env.SLACK_HNMONITOR_WEBHOOK_URL;
 const webhook = new IncomingWebhook(webhookurl);
 
 source = "http://api.hnstream.com/comments/stream/";
-
 
 target = argv.word;
 
@@ -31,13 +30,15 @@ var skipNoMatch = filter({ objectMode: true }, function(chunk) {
   );
 });
 
-// var dumpchunk = spy({ objectMode: true },function(chunk) {
-//   console.log("Dump");
-//   console.log(JSON.stringify(chunk, null, " "));
-// });
 
-strm.pipeline(
-  hyperquest(source),
+
+var heartbeat = spy({ objectMode: false }, function(chunk) {
+  console.log(".");
+});
+
+stream.pipeline(
+  request(source),
+  heartbeat,
   ndjson.parse({ strict: false }),
   skipNoMatch,
   through2.obj(post),
@@ -50,15 +51,19 @@ strm.pipeline(
   }
 );
 
-function idToItemLink(id,text) {
-  return `<https://news.ycombinator.com/item?id=${id}|${entities.decodeHTML(text)}>`;
+function idToItemLink(id, text) {
+  return `<https://news.ycombinator.com/item?id=${id}|${entities.decodeHTML(
+    text
+  )}>`;
 }
 
 function idToUserLink(id) {
-  return `<https://news.ycombinator.com/user?id=${id}|${entities.decodeHTML(id)}>`;
+  return `<https://news.ycombinator.com/user?id=${id}|${entities.decodeHTML(
+    id
+  )}>`;
 }
 
-function post(row, enc, next) {
+function post(row, enc, cb) {
   console.log("Posting")
   msg="In *"+idToItemLink(row["article-id"],row["article-title"])+"* _"+idToUserLink(row.author)+"_ "+idToItemLink(row.id,"said")+"\n"+slackify(row.body);
   webhook.send(msg, (err, res) => {
@@ -68,6 +73,12 @@ function post(row, enc, next) {
       console.log("Sent:", row);
     }
     console.log("Posted")
-    next(null, row);
+    cb();
   });
 }
+
+
+// var dumpchunk = spy({ objectMode: true },function(chunk) {
+//   console.log("Dump");
+//   console.log(JSON.stringify(chunk, null, " "));
+// });
