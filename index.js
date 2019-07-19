706060c8bd572e273584e5af3314b76580b8bc5c
Dj Walker-Morgan <djwm@mongodb.com>
2019-07-19 11:21:33 +0100

    Updated with bold pattern spotting

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var filter = require("through2-filter");
 var spy = require("through2-spy");
 var entities = require("entities");
 var slackify = require("slackify-html");
-var Sentiment = require('sentiment');
+var Sentiment = require("sentiment");
 var sentiment = new Sentiment();
 
 var webhookurl = process.env.SLACK_HNMONITOR_WEBHOOK_URL;
@@ -24,24 +24,25 @@ const webhook = new IncomingWebhook(webhookurl);
 
 source = "http://api.hnstream.com/comments/stream/";
 
-target = argv.word;
-targetRegexp = null;
+target = argv.word;  // word is what we are looking for
+targetRegexp = null; // if target is a regexp, used for matching
+subRegexp = null;    // used for replacing not matching
 
-if(target.startsWith("/") && target.endsWith("/")) {
+if (target.startsWith("/") && target.endsWith("/")) {
   // Matching against a regexp
-  slice=target.slice(1,-1)
-  targetRegexp=new RegExp(slice,'i')
-  console.log("Will look for regular expression ${slice}")
+  slice = target.slice(1, -1);
+  targetRegexp = new RegExp(slice, "i");
+  console.log(`Will look for regular expression ${slice}`);
 } else {
-  console.log(`Will look for ${target}`);
+  console.log(`Will look for ${target}`);ß
 }
 
 var skipNoMatch = filter({ objectMode: true }, function(chunk) {
-  if(targetRegexp==null) {
-  return (
-    chunk.body.toLowerCase().includes(target) ||
-    chunk["article-title"].toLowerCase().includes(target)
-  );
+  if (targetRegexp == null) {
+    return (
+      chunk.body.toLowerCase().includes(target) ||
+      chunk["article-title"].toLowerCase().includes(target)
+    );
   } else {
     return (
       chunk.body.match(targetRegexp) ||
@@ -50,17 +51,17 @@ var skipNoMatch = filter({ objectMode: true }, function(chunk) {
   }
 });
 
-var countskips=0;
+var countskips = 0;
 
 var heartbeat = spy({ objectMode: false }, function(chunk) {
   countskips++;
 });
 
 var heartbeatreset = spy({ objectMode: true }, function(chunk) {
-  if(countskips!=0) {
+  if (countskips != 0) {
     console.log(`Skipped ${countskips}`);
   }
-  countskips=0;
+  countskips = 0;
 });
 
 stream.pipeline(
@@ -94,36 +95,49 @@ function idToUserLink(id) {
 
 function post(row, enc, cb) {
   // console.log("Posting")
-  emoji=":neutral_face:";
-  if(row.score<-1) {
-    emoji=":angry:";
-  } else if (row.score>1) {
-    emoji=":simple_smile:";
+  emoji = ":neutral_face:";
+  if (row.score < -1) {
+    emoji = ":angry:";
+  } else if (row.score > 1) {
+    emoji = ":simple_smile:";
   }
-  msg=`${emoji} ${row.score} - *${idToItemLink(row["article-id"],row["article-title"])}* _${idToUserLink(row.author)}_ ${idToItemLink(row.id,"said")}\n${slackify(row.body)}`;
-  // console.log(row.body);
-  // console.log(slackify(row.body));
-  // cb();
-  webhook.send(msg, (err, res) => {
-    if (err) {
-      console.log("Error:", err);
-    } 
-    else {
-      console.log("Sent:", row);
+
+  if (targetRegexp == null) {
+    if (subRegexp==null) {
+      subRegexp=new RegExp(target,"i");
     }
-    console.log("Posted")
-    cb();
-  });
+    body = row.body.replace(subRegexp ,"<b>$&</b>");
+  } else {
+    body = row.body.replace(targetRegexp, "<b>$&</b>");
+  }
+  msg = `${emoji} ${row.score} - *${idToItemLink(
+    row["article-id"],
+    row["article-title"]
+  )}* _${idToUserLink(row.author)}_ ${idToItemLink(row.id, "said")}\n${slackify(
+    body
+  )}`;
+  console.log(body);
+  cb();
+
+  // webhook.send(msg, (err, res) => {
+  //   if (err) {
+  //     console.log("Error:", err);
+  //   }
+  //   else {
+  //     console.log("Sent:", row);
+  //   }
+  //   console.log("Posted")
+  //   cb();
+  // });
 }
 
-function sentimental(row,enc,cb) {
-  result=sentiment.analyze(row.body);
-  row.score=result.score;
-  cb(null,row);
+function sentimental(row, enc, cb) {
+  result = sentiment.analyze(row.body);
+  row.score = result.score;
+  cb(null, row);
 }
 
 // var dumpchunk = spy({ objectMode: true },function(chunk) {
 //   console.log("Dump");
 //   console.log(JSON.stringify(chunk, null, " "));
 // });
-
